47077ee048e9dfcf5aaf693010a81bb35fcd00fa
Alon Cohen Magen <alloncm@gmail.com>
2017-10-24 23:59:59 +0300

    added Score

---
+++ learnning/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    float timer = 0;
+    float timeIncSpeed = 3;
+    float incSpeed = -0.1f;
+
+    public Vector3 obsSpeed;
+
+    GUIText text;
+
+    float ScoreTime = 0.0f;
+	// Use this for initialization
+	void Start ()
+    {
+        obsSpeed = new Vector3(0, 0, -15);
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(obsSpeed);
+        if (timer > timeIncSpeed)
+        {
+            timer = 0;
+            obsSpeed.z += incSpeed;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+        ScoreTime += Time.deltaTime;
+        UpdateGui();
+    }
+    public Vector3 GetObsSpeed()
+    {
+        return obsSpeed;    
+    }
+
+    public void UpdateGui()
+    {
+        text = GetComponent<GUIText>();
+        text.text = ((ScoreTime)).ToString("0.0");
+    }
+}
---
+++ learnning/Assets/Scripts/RespawnObjects.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObjects : MonoBehaviour {
+
+    public GameObject cube;
+    private float timer = 0;
+    float destTime = 0.5f;
+    float decTime = 0.001f;
+    float minDestTime = 0.2f;
+    // Use this for initialization
+    void Start ()
+    {
+        float r = Random.Range(-10, 10);
+        Instantiate(cube, new Vector3(r, 1, 15), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer > destTime)
+        {
+           
+            DecreaseTimer();
+            int scale = Random.Range(1, 5);
+            int numobs = 2;
+            if(scale<3)
+            {
+                numobs = 4;
+            }
+            for (int i = 0; i < numobs; i++)
+            {
+                float r = Random.Range(-10, 10);
+
+                cube.transform.localScale = new Vector3(scale, 1, 1);
+                Instantiate(cube, new Vector3(r, 1, 15), Quaternion.identity);
+            }
+            timer = 0;
+        }
+    }
+    void DecreaseTimer()
+    {
+        if((destTime-decTime)>minDestTime)
+        {
+            destTime -= decTime;
+        }
+    }
+}
